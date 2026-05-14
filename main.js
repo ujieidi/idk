173fb8d2793c45ffc35b4a26411af8de115a56e4
ujieidi <ujieidi@user.noreply.github.com>
2026-05-14 12:35:08 +0900

    selling system

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ let stats = {
 
 const miningButton = document.getElementById("miningButton");
 const smeltingButton = document.getElementById("smeltingButton");
+const sellButton = document.getElementById("sellButton")
 
 const oreCount = document.getElementById("ore-count");
 const barCount = document.getElementById("bar-count");
+const moneyCount = document.getElementById("money-count")
 
 function updateDisplay(){
   oreCount.textContent = stats.copperOre;
   barCount.textContent = stats.copperBar;
-  
+  moneyCount.textContent = stats.money;
 };
 
 function mineCopper(){
@@ -27,10 +29,25 @@ function smeltCopper(){
     stats.copperBar++;
   }
   else{
-    console.log("needs atleast 1 copperOre to smelt")
+    console.log("not enough copperOre")
   }
   updateDisplay();
- };
+};
+ 
+function sell(){
+  if (stats.copperBar >= 1){
+    stats.copperBar--;
+    stats.money += 10;
+  }
+  else {
+    console.log("not enough copperBar")
+  };
+  updateDisplay();
+};
   
 miningButton.addEventListener("click", mineCopper);
 smeltingButton.addEventListener("click", smeltCopper);
+sellButton.addEventListener("click", sell  )
+
+
+//i will not be using abbreviation mostly since it may confuse myself
